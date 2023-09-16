7351b97014bd3900305c014a5bcc9f683a303409
Glare <admin@glaremasters.me>
2023-09-16 09:54:14 -0500

    fix: move meta tags to build time properly

--- app.vue
+++ app.vue
@@ -2,25 +2,6 @@
 import { useConstantStore } from './stores/constants'
 import { useDefaultStore } from './stores/defaults'
 
-const title = 'MCBanners - Minecraft Statistical Banners'
-const description = 'We turn backend statistics into beautiful front-end images that can be displayed on forums and more.'
-const imageUrl = 'https://mcbanners-overhaul.pages.dev/logo_transparent_cropped.png'
-
-useHead({
-  title,
-  meta: [
-    { hid: 'description', name: 'description', content: description },
-    { hid: 'twitter:title', name: 'twitter:title', content: title },
-    { hid: 'twitter:description', name: 'twitter:description', content: description },
-    { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
-    { hid: 'twitter:image', name: 'twitter:image', content: imageUrl },
-    { hid: 'og:description', property: 'og:description', content: description },
-    { hid: 'og:image', property: 'og:image', content: imageUrl },
-    { hid: 'og:image:type', property: 'og:image:type', content: 'image/png' },
-    { hid: 'og:type', property: 'og:type', content: 'website' }
-  ]
-})
-
 const constants = useConstantStore()
 const defaults = useDefaultStore()
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const title = 'MCBanners - Minecraft Statistical Banners'
+const description = 'We turn backend statistics into beautiful front-end images that can be displayed on forums and more.'
+const imageUrl = 'https://mcbanners-overhaul.pages.dev/logo_transparent_cropped.png'
+
 export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
@@ -11,5 +16,23 @@ export default defineNuxtConfig({
   },
   site: {
     url: 'https://mcbanners-overhaul.pages.dev/'
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title,
+      meta: [
+        { hid: 'description', name: 'description', content: description },
+        { hid: 'twitter:title', name: 'twitter:title', content: title },
+        { hid: 'twitter:description', name: 'twitter:description', content: description },
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'twitter:image', name: 'twitter:image', content: imageUrl },
+        { hid: 'og:description', property: 'og:description', content: description },
+        { hid: 'og:image', property: 'og:image', content: imageUrl },
+        { hid: 'og:image:type', property: 'og:image:type', content: 'image/png' },
+        { hid: 'og:type', property: 'og:type', content: 'website' }
+      ]
+    }
   }
 })
