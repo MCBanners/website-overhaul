a264933791218591e3790970a68a8855bba87ebd
Glare <admin@glaremasters.me>
2023-09-16 09:39:07 -0500

    feat: improve runtime seo

--- app.vue
+++ app.vue
@@ -2,6 +2,18 @@
 import { useConstantStore } from './stores/constants'
 import { useDefaultStore } from './stores/defaults'
 
+const title = 'MCBanners - Minecraft Statistical Banners'
+const description = 'We turn backend statistics into beautiful front-end images that can be displayed on forums and more.'
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+  ogImage: 'https://mcbanners-overhaul.pages.dev/logo_transparent_cropped.png',
+  twitterCard: 'summary_large_image'
+})
+
 const constants = useConstantStore()
 const defaults = useDefaultStore()
 
@@ -12,7 +24,6 @@ await defaults.getDefaults()
 <template>
   <div class="bg-gray-100 dark:bg-gray-900 h-screen">
     <NuxtLayout>
-      <SeoKit />
       <NuxtPage />
     </NuxtLayout>
     <UNotifications />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [
-    'nuxt-seo-kit'
-  ],
   ssr: false,
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt'],
+  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxtjs/robots', 'nuxt-simple-sitemap'],
   experimental: {
     viewTransition: true
   },
   ui: {
     icons: ['heroicons', 'simple-icons']
   },
-  runtimeConfig: {
-    public: {
-      siteUrl: 'https://mcbanners-overhaul.pages.dev',
-      siteName: 'MCBanners',
-      siteDescription: 'We turn backend statistics into beautiful front-end images that can be displayed on forums and more.',
-      language: 'en-US'
-    }
+  site: {
+    url: 'https://mcbanners-overhaul.pages.dev/'
   }
 })
