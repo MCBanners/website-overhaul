ba940fa33945968de39b078252a3fb342d96f44f
Glare <admin@glaremasters.me>
2023-09-16 09:46:48 -0500

    chore: clean up meta tags

--- app.vue
+++ app.vue
@@ -4,14 +4,21 @@ import { useDefaultStore } from './stores/defaults'
 
 const title = 'MCBanners - Minecraft Statistical Banners'
 const description = 'We turn backend statistics into beautiful front-end images that can be displayed on forums and more.'
+const imageUrl = 'https://mcbanners-overhaul.pages.dev/logo_transparent_cropped.png'
 
-useSeoMeta({
+useHead({
   title,
-  ogTitle: title,
-  description,
-  ogDescription: description,
-  ogImage: 'https://mcbanners-overhaul.pages.dev/logo_transparent_cropped.png',
-  twitterCard: 'summary_large_image'
+  meta: [
+    { hid: 'description', name: 'description', content: description },
+    { hid: 'twitter:title', name: 'twitter:title', content: title },
+    { hid: 'twitter:description', name: 'twitter:description', content: description },
+    { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+    { hid: 'twitter:image', name: 'twitter:image', content: imageUrl },
+    { hid: 'og:description', property: 'og:description', content: description },
+    { hid: 'og:image', property: 'og:image', content: imageUrl },
+    { hid: 'og:image:type', property: 'og:image:type', content: 'image/png' },
+    { hid: 'og:type', property: 'og:type', content: 'website' }
+  ]
 })
 
 const constants = useConstantStore()
